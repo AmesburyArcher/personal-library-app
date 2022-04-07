8388d5093a68a8e134775e0e2916d13ee40f27c3
Max C <smashmouthmax@gmail.com>
2022-04-07 10:28:22 -0700

    add book button now displays book object values on the DOM, need to create the remove book function

--- script.js
+++ script.js
@@ -24,16 +24,55 @@ function Book(title, author, pages, read) {
 const submitBookBtn = document.querySelector('.submit-book');
 submitBookBtn.addEventListener('click', addBookToLibrary);
 
+
 function addBookToLibrary(ev) {
     ev.preventDefault();
+    // takes in input from users to create a new book object that gets stored in myLibrary array
         let title = document.querySelector('#title-name').value;
         let author = document.querySelector('#author-name').value;
         let pages = document.querySelector('#num-pages').value;
         let read = document.querySelector('#num-pages-read').value
 
         let book = new Book(title, author, pages, read)
-        myLibrary.push(book);
-        console.log(myLibrary);
+        if(title.value && author.value && pages.value && read.value) {
+            myLibrary.push(book);
+             // takes the property values from book object and displays it on the DOM in a book card    
+            const bookHolder = document.querySelector('#book-holder');
+            const createCard = document.createElement('div');
+            const deleteBtn = document.createElement('p');
+            const bookTitle = document.createElement('p');
+            const bookAuthor = document.createElement('p');
+            const bookTotalPages = document.createElement('p');
+            const bookPagesRead = document.createElement('p');
+
+            deleteBtn.classList.add('delete-book');
+            deleteBtn.textContent = 'x';
+            createCard.classList.add('book-card');
+            bookTitle.textContent = "Title: " + title;
+            bookAuthor.textContent = "Author: " + author;
+            bookTotalPages.textContent = "Total Pages in Book: " + pages;
+            bookPagesRead.textContent = "Total Pages Read: " + read;
+
+            bookHolder.appendChild(createCard);
+            createCard.appendChild(deleteBtn);
+            createCard.appendChild(bookTitle);
+            createCard.appendChild(bookAuthor);
+            createCard.appendChild(bookTotalPages);
+            createCard.appendChild(bookPagesRead);
+
+            console.log(myLibrary);
+            document.querySelector('.book-info').reset();
+        } else {
+            alert('You must enter all fields');
+        }
 
 };
+
+
+// const deleteBookBtn = document.querySelector('.delete-book');
+// deleteBookBtn.addEventListener('click', (e) => {
+
+// })
+
+
 
